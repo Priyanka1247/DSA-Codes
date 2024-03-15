30c743c7a4bf69c66751b369d707497e0448ce9e
Priyanka1247 <131129829+Priyanka1247@users.noreply.github.com>
2024-03-15 23:03:55 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+unsigned long long factorial(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    unsigned long long fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0)
+        printf("Factorial of a negative number is undefined.\n");
+    else {
+        fact = factorial(num);
+        printf("Factorial of %d = %llu\n", num, fact);
+    }
+
+    return 0;
+}
