cb0d217958982141fcf8282649012a73165d2aa6
Priyanka1247 <131129829+Priyanka1247@users.noreply.github.com>
2024-03-15 23:23:27 +0530

    Add files via upload

---
+++ sum of digits.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int sumOfDigits(int num) {
+    int sum = 0;
+
+
+    while (num != 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    return sum;
+}
+
+int main() {
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    int sum = sumOfDigits(number);
+
+    printf("Sum of digits of %d = %d\n", number, sum);
+
+    return 0;
+}
+
+
